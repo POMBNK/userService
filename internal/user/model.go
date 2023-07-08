13574281c8b191516f54509e53bdfc860040ff2d
POMBNK <shardex11@gmail.com>
2023-07-08 20:04:26 +0300

    DTO to Create and Update methods

--- internal/user/model.go
+++ internal/user/model.go
@@ -7,8 +7,32 @@ type User struct {
 	Email        string `json:"email" bson:"email"`
 }
 
-type UserDTO struct {
-	UserName     string `json:"username" `
-	PasswordHash string `json:"password"`
-	Email        string `json:"email"`
+type ToCreateUserDTO struct {
+	UserName     string `json:"username" bson:"username"`
+	PasswordHash string `json:"-" bson:"password"`
+	Email        string `json:"email" bson:"email"`
+}
+
+type ToUpdateUserDTO struct {
+	ID           string `json:"ID" bson:"_id,omitempty"`
+	UserName     string `json:"username" bson:"username"`
+	PasswordHash string `json:"-" bson:"password"`
+	Email        string `json:"email" bson:"email"`
+}
+
+func CreateUserDto(dto ToCreateUserDTO) User {
+	return User{
+		UserName:     dto.UserName,
+		PasswordHash: dto.PasswordHash,
+		Email:        dto.Email,
+	}
+}
+
+func UpdateUserDto(dto ToUpdateUserDTO) User {
+	return User{
+		ID:           dto.ID,
+		UserName:     dto.UserName,
+		PasswordHash: dto.PasswordHash,
+		Email:        dto.Email,
+	}
 }
